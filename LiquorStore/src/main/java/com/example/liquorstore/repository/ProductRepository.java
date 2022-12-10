b3ef7fcbcbaf0d7e233c929c94457773fedd216c
DanielxFDA <floredaniel47@gmail.com>
2022-12-10 14:10:26 +0200

    commit 5

--- LiquorStore/src/main/java/com/example/liquorstore/repository/ProductRepository.java
+++
@@ -1,9 +0,0 @@
-package com.example.liquorstore.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Query;
-
-public interface ProductRepository extends JpaRepository<Product, Long>{
-    @Query("SELECT p FROM Product p WHERE p.productName = ?1")
-    public Product findByProductName(String productName);
-}
